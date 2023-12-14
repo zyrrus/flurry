47b80dfad801152d84c50539f4b118703a51917e
Zeke Abshire <zekeabshire@gmail.com>
2023-12-13 23:44:42 -0600

    feat: lesson api

--- src/server/api/routers/lesson.ts
+++ src/server/api/routers/lesson.ts
@@ -1,9 +1,28 @@
 import { eq } from "drizzle-orm";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { lessons } from "~/server/db/schema";
 
-export const lessonRouter = createTRPCRouter({});
+export const lessonRouter = createTRPCRouter({
+  getAllCourseLessons: protectedProcedure
+    .input(z.object({ courseId: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.query.lessons.findMany({
+        where: eq(lessons.courseId, input.courseId),
+      });
+    }),
+  getLesson: protectedProcedure
+    .input(
+      z.object({
+        lessonId: z.number(),
+        includeTopics: z.boolean().optional().default(false),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const topics = input.includeTopics ? true : undefined;
+      return await ctx.db.query.lessons.findFirst({
+        where: eq(lessons.lessonId, input.lessonId),
+        with: { topics },
+      });
+    }),
+});
